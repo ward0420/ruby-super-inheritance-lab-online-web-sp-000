baf1b375edf38ed8a950e8e7100bb459e3991aba
Don Ward <10is0420@gmail.com>
2019-08-23 19:22:33 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,6 +2,6 @@ class Chatty_student < Student
   
   def hello
     super
-    puts"
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
     
 end
