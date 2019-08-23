1c064d36c75b1f235ea7c6c762754cecc54f964a
Don Ward <10is0420@gmail.com>
2019-08-23 19:34:00 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,7 +7,8 @@ class ChattyStudent < Student
   
   def raise_hand
     super
-      puts "Pick me!Pick me!Pick me!Pick me!Pick me!Pick me!Pick me!Pick me!Pick me!"
+      puts "Pick me!
+      Pick me!Pick me!Pick me!Pick me!Pick me!Pick me!Pick me!Pick me!"
   end
     
 end
